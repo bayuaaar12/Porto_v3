8e8d45b5d8f8a54fc7c13a981000780c5b72af0e
bayuaaar12 <akuntv397@gmail.com>
2026-09-23 10:44:40 +0700

    perf: configure security headers and HTTP optimization rules in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,31 @@ const nextConfig = {
   images: {
     domains: ['api.dicebear.com', 'raw.githubusercontent.com', 'images.unsplash.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
